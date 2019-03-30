0e0d0926799a5bcae558d94426031792123697d3
Austin Gruenberg <gruenberau@mnstate.edu>
2019-03-30 13:51:21 -0500

    created test for createMenu, added doc

--- createMenu.py
+++ createMenu.py
@@ -1,5 +1,10 @@
 #
 def createMenu(optionList):
+  """
+  precondition: optionList is of list type
+  postcondition: items in optionList is added to tmp
+  description: takes items from optionList and organizes them into a menu string
+  """
   tmp = ' ': ct = 0
   for opt in optionList:
     tmp+=str(ct) + '.' + opt + '\n'
---
+++ testMenu.py
@@ -0,0 +1,6 @@
+optionsList=["Add","Subtract","Multiply","Divide","Quit"]
+creatMenu(optionsList)
+optionsList2=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+createMenu(optionsList2)
+optionsList3=["Ham","Turkey","Roast Beef","Chicken","Egg Salad"]
+createMenu(optionsList3)
